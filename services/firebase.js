b74d359706a2b5bb674656ecdd672f8c192be114
Tyler-Seliber <tylerseliber@gmail.com>
2022-03-17 18:38:22 -0400

    Added firebase config

---
+++ services/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+import Constants from "expo-constants";
+const firebaseConfig = {
+    apiKey: "Constants.manifest?.extra?.firebaseApiKey",
+    authDomain: "Constants.manifest?.extra?.firebaseAuthDomain",
+    projectId: "Constants.manifest?.extra?.firebaseProjectId",
+    storageBucket: "Constants.manifest?.extra?.firebaseStorageBucket",
+    messagingSenderId: "Constants.manifest?.extra?.firebaseMessagingSenderId",
+    appId: "Constants.manifest?.extra?.firebaseAppId",
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
